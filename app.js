db9569a854e591c35fca24a07c51cfcb48a7a356
ahazrat <asifhazrat@gmail.com>
2021-04-10 15:23:27 -0500

    fix errors

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 
-function createTable(d) {
-    table = d3.select('#table')
-    thead = table.append('thead')
-    tbody = table.append('tbody')
-    thead.append
-    console.log(thead)
-}
+// function createTable(d) {
+//     table = d3.select('#table')
+//     thead = table.append('thead')
+//     tbody = table.append('tbody')
+//     thead.append
+//     console.log(thead)
+// }
 
-d3.json('data.json').then(data => {
+// d3.json('data.json').then(data => {
 
-    console.log(data)
-    createTable(data)
-})
+//     console.log(data)
+//     createTable(data)
+// })
